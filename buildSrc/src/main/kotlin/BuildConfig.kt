21659023e8b028e01d4a46ae6289d0961bd73f4a
AutumnPizazz <autumnpizazz@users.noreply.github.com>
2026-02-23 16:20:22 +0800

    修复我也不懂的问题

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -5,5 +5,5 @@ object BuildConfig {
     const val appName = "Unciv"
     const val appCodeNumber = 1203
     const val appVersion = "4.19.14-patch1"
-    const val identifier = "com.unciv.app.cn"
+    const val identifier = "com.unciv.app"
 }
