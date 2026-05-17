63960ba05e0296fa5e48c7e4ed407f96ed77d569
AutumnPizazz <autumnpizazz@users.noreply.github.com>
2026-05-17 14:01:51 +0800

    更新版本号

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,6 +4,6 @@ package com.unciv.build
 object BuildConfig {
     const val appName = "Unciv"
     const val appCodeNumber = 1218
-    const val appVersion = "4.20.6.2"
+    const val appVersion = "4.20.6.3"
     const val identifier = "com.unciv.app"
 }
