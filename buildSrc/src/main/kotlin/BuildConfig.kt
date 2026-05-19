23ac6b9c784c0f9317703ecdbd949cfadb823308
AutumnPizazz <autumnpizazz@users.noreply.github.com>
2026-05-19 21:40:42 +0800

    4.20.7.4
     - 新增json扩展系统文档说明
     - 镜像地图让资源也对称

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,6 +4,6 @@ package com.unciv.build
 object BuildConfig {
     const val appName = "Unciv"
     const val appCodeNumber = 1219
-    const val appVersion = "4.20.7.3"
+    const val appVersion = "4.20.7.4"
     const val identifier = "com.unciv.app"
 }
