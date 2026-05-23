887c089a6727ad1234b812e1f2573716582d5fd7
AutumnPizazz <autumnpizazz@users.noreply.github.com>
2026-05-23 19:40:20 +0800

    4.20.8.3

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,6 +4,6 @@ package com.unciv.build
 object BuildConfig {
     const val appName = "Unciv"
     const val appCodeNumber = 1219
-    const val appVersion = "4.20.8.2"
+    const val appVersion = "4.20.8.3"
     const val identifier = "com.unciv.app"
 }
