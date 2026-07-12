d434f667778201db073c1d62030fde10f160310b
AutumnPizazz <autumnpizazz@users.noreply.github.com>
2026-07-12 23:18:41 +0800

    4.20.17.2
    - 新增地图格备注与单位备注功能
    - 取消旋转对称地图对半径的奇偶限制
    - 禁用文明积分面板时同步禁用实验性统计面板
    - 实现游戏版本兼容性隔离（旧版无法加载新版存档）
    - 训练移民等食物->产能单位时避免市民饿死

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,6 +4,6 @@ package com.unciv.build
 object BuildConfig {
     const val appName = "Unciv"
     const val appCodeNumber = 1232
-    const val appVersion = "4.20.17.1"
+    const val appVersion = "4.20.17.2"
     const val identifier = "com.unciv.app"
 }
