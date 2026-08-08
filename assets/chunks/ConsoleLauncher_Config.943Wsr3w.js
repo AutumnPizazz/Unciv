7d6b2680921c0be324d28f68323ed4ffea283fe2
AutumnPizazz <AutumnPizazz@users.noreply.github.com>
2026-08-08 20:52:32 +0000

    deploy: fef1fc42325de12d48bd13c697357125263fb646

---
+++ assets/chunks/ConsoleLauncher_Config.943Wsr3w.js
@@ -0,0 +1,1 @@
+const s="/Unciv/assets/ConsoleLauncher_Config.Bq5bT7f2.png";export{s as _};
---
+++ assets/chunks/Desktop_Build.CQHeU67k.js
@@ -0,0 +1,1 @@
+const s="/Unciv/assets/Android_SDK_Platforms.QCTLUiYz.png",o="/Unciv/assets/Android_SDK_Tools.BhZZRuGC.png",t="/Unciv/assets/Desktop_Build.b3FCiADf.png";export{s as _,o as a,t as b};
